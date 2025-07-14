ac8a55da77474a419ccc6f6a1b034250f1743eb6
Pranto Deb <rajprit0m@outlook.com>
2025-07-15 01:58:16 +0600

    #

--- index.js
+++ index.js
@@ -13,43 +13,49 @@ const ai = new GoogleGenAI({
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send({ data: "Server is running", status: 200 });
-});
-
-async function generateTextWithGemini(prompt) {
+const generateTextWithGemini = async (prompt) => {
   try {
-    const model = ai.getGenerativeModel({ model: "gemini-2.5-flash" });
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
 
-    const result = await model.generateContent(prompt);
-
-    const response = await result.response;
-
-    const text = response.text();
+    const text = response.text;
     return text;
   } catch (error) {
     console.error("Error calling Gemini API:", error);
     throw new Error("Failed to generate text from AI. Please try again later.");
   }
-}
+};
 
-app.post("/generate-text", async (req, res) => {
-  const { prompt } = req.body;
-
-  if (!prompt) {
-    return res
-      .status(400)
-      .json({ error: "Prompt is required in the request body." });
-  }
-
-  try {
-    const generatedText = await generateTextWithGemini(prompt);
-    res.json({ generatedText: generatedText });
-  } catch (error) {
-    console.error("Error in /generate-text route:", error.message);
-    res.status(500).json({ error: error.message });
-  }
+app.get("/", (req, res) => {
+  res.send({ data: "Server is running", status: 200 });
 });
+
+app.get("/prompt", async (req, res) => {
+  const prompt = "How does AI work?";
+  const response = await generateTextWithGemini(prompt);
+
+  res.send({ data: response, status: 200 });
+});
+
+// app.post("/generate-text", async (req, res) => {
+//   const { prompt } = req.body;
+
+//   if (!prompt) {
+//     return res
+//       .status(400)
+//       .json({ error: "Prompt is required in the request body." });
+//   }
+
+//   try {
+//     const generatedText = await generateTextWithGemini(prompt);
+//     res.json({ generatedText: generatedText });
+//   } catch (error) {
+//     console.error("Error in /generate-text route:", error.message);
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
