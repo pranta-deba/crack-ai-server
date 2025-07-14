241522090ba02d91c481332b29b8e36e6c86984f
Pranto Deb <rajprit0m@outlook.com>
2025-07-15 03:16:42 +0600

    db connect

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
---
+++ utils/db.connect.js
@@ -0,0 +1,27 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.DB_URL;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db = null;
+
+const connect = async () => {
+  try {
+    await client.connect();
+    db = client.db("crack-ai");
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+};
+
+module.exports = { connect, db };
