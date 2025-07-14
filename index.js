3eb89aa43771a10a8507c94f5cf4d10981bcb8b1
Pranto Deb <rajprit0m@outlook.com>
2025-07-14 23:58:02 +0600

    #

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
 
+const GoogleGenAI = require("@google/genai");
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY
+});
+
 app.get("/", (req, res) => {
   res.send({ data: "Server is running", status: 200 });
 });
